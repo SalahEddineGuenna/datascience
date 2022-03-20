86284c78ead52a7dce0926bb4d3cfeeb668264c8
salah eddine <salimali1768@gmail.com>
2022-03-20 11:32:01 +0100

    The Android App Market on Google Play

--- Google Play/main.py
+++ Google Play/main.py
@@ -3,11 +3,13 @@
 import pandas as pd
 import numpy as np
 import plotly
-plotly.offline.init_notebook_mode(connected=True)
+
 import plotly.graph_objs as go
 import seaborn as sns
+
 sns.set_style("darkgrid")
 import warnings
+
 warnings.filterwarnings("ignore")
 import matplotlib.pyplot as plt
 
@@ -33,7 +35,7 @@ for col in cols_to_clean:
     for char in chars_to_remove:
         # Replace the character with an empty string
         apps[col] = apps[col].apply(lambda x: x.replace(char, ''))
-        
+
 # Print a summary of the apps dataframe
 print(apps.info())
 
@@ -54,11 +56,11 @@ print('Number of categories = ', num_categories)
 num_apps_in_category = apps['Category'].value_counts()
 
 # Sort num_apps_in_category in descending order based on the count of apps in each category
-sorted_num_apps_in_category = num_apps_in_category.sort_values(ascending = False)
+sorted_num_apps_in_category = num_apps_in_category.sort_values(ascending=False)
 
 data = [go.Bar(
-        x = num_apps_in_category.index, # index = category name
-        y = num_apps_in_category.values, # value = count
+    x=num_apps_in_category.index,  # index = category name
+    y=num_apps_in_category.values,  # value = count
 )]
 
 plotly.offline.iplot(data)
@@ -69,19 +71,19 @@ print('Average app rating = ', avg_app_rating)
 
 # Distribution of apps according to their ratings
 data = [go.Histogram(
-        x = apps['Rating']
+    x=apps['Rating']
 )]
 
 # Vertical dashed line to indicate the average app rating
 layout = {'shapes': [{
-              'type' :'line',
-              'x0': avg_app_rating,
-              'y0': 0,
-              'x1': avg_app_rating,
-              'y1': 1000,
-              'line': { 'dash': 'dashdot'}
-          }]
-          }
+    'type': 'line',
+    'x0': avg_app_rating,
+    'y0': 0,
+    'x1': avg_app_rating,
+    'y1': 1000,
+    'line': {'dash': 'dashdot'}
+}]
+}
 
 plotly.offline.iplot({'data': data, 'layout': layout})
 
@@ -92,13 +94,13 @@ apps_with_size_and_rating_present = apps[(~apps['Rating'].isnull()) & (~apps['Si
 large_categories = apps_with_size_and_rating_present.groupby('Category').filter(lambda x: len(x) >= 250)
 
 # Plot size vs. rating
-plt1 = sns.jointplot(x = large_categories['Size'], y = large_categories['Rating'])
+plt1 = sns.jointplot(x=large_categories['Size'], y=large_categories['Rating'])
 
 # Select apps whose 'Type' is 'Paid'
 paid_apps = apps_with_size_and_rating_present[apps_with_size_and_rating_present['Type'] == 'Paid']
 
 # Plot price vs. rating
-plt2 = sns.jointplot(x = paid_apps['Price'], y = paid_apps['Rating'])
+plt2 = sns.jointplot(x=paid_apps['Price'], y=paid_apps['Rating'])
 
 fig, ax = plt.subplots()
 fig.set_size_inches(15, 8)
@@ -106,10 +108,10 @@ fig.set_size_inches(15, 8)
 # Select a few popular app categories
 popular_app_cats = apps[apps.Category.isin(['GAME', 'FAMILY', 'PHOTOGRAPHY',
                                             'MEDICAL', 'TOOLS', 'FINANCE',
-                                            'LIFESTYLE','BUSINESS'])]
+                                            'LIFESTYLE', 'BUSINESS'])]
 
 # Examine the price trend by plotting Price vs Category
-ax = sns.stripplot(x = popular_app_cats['Price'], y = popular_app_cats['Category'], jitter=True, linewidth=1)
+ax = sns.stripplot(x=popular_app_cats['Price'], y=popular_app_cats['Category'], jitter=True, linewidth=1)
 ax.set_title('App pricing trend across categories')
 
 # Apps whose Price is greater than 200
@@ -123,26 +125,27 @@ fig, ax = plt.subplots()
 fig.set_size_inches(15, 8)
 
 # Examine price vs category with the authentic apps (apps_under_100)
-ax = sns.stripplot(x = apps_under_100['Price'], y = apps_under_100['Category'], data = apps_under_100, jitter = True, linewidth = 1)
+ax = sns.stripplot(x=apps_under_100['Price'], y=apps_under_100['Category'], data=apps_under_100, jitter=True,
+                   linewidth=1)
 ax.set_title('App pricing trend across categories after filtering for junk apps')
 
 trace0 = go.Box(
     # Data for paid apps
-    y = apps[apps['Type'] == 'Paid']['Installs'],
-    name = 'Paid'
+    y=apps[apps['Type'] == 'Paid']['Installs'],
+    name='Paid'
 )
 
 trace1 = go.Box(
     # Data for free apps
-    y = apps[apps['Type'] == 'Free']['Installs'],
-    name = 'Free'
+    y=apps[apps['Type'] == 'Free']['Installs'],
+    name='Free'
 )
 
 layout = go.Layout(
-    title = "Number of downloads of paid apps vs. free apps",
-    yaxis = dict(title = "Log number of downloads",
-                type = 'log',
-                autorange = True)
+    title="Number of downloads of paid apps vs. free apps",
+    yaxis=dict(title="Log number of downloads",
+               type='log',
+               autorange=True)
 )
 
 # Add trace0 and trace1 to a list for plotting
@@ -156,12 +159,12 @@ reviews_df = pd.read_csv("datasets/user_reviews.csv")
 merged_df = apps.merge(reviews_df)
 
 # Drop NA values from Sentiment and Review columns
-merged_df = merged_df.dropna(subset = ['Sentiment', 'Review'])
+merged_df = merged_df.dropna(subset=['Sentiment', 'Review'])
 
 sns.set_style('ticks')
 fig, ax = plt.subplots()
 fig.set_size_inches(11, 8)
 
 # User review sentiment polarity for paid vs. free apps
-ax = sns.boxplot(x = merged_df['Type'], y = merged_df['Sentiment_Polarity'], data = merged_df)
+ax = sns.boxplot(x=merged_df['Type'], y=merged_df['Sentiment_Polarity'], data=merged_df)
 ax.set_title('Sentiment Polarity Distribution')
